05cdc012a93a88238ec054ecfd89e8c129627c8b
mitragroups <miftahmuhajir@gmail.com>
2018-11-15 05:23:30 +0700

    Menambahkan file cara ngegit yang gampang

---
+++ carangegit.rb
@@ -0,0 +1,39 @@
+# cek git version 
+
+puts "git --version"
+
+#buat folder dengan cara
+
+puts "mkdir nama-folder"
+
+#arahkan ke folder yang anda buat misalnya
+
+puts "cd belajar-git"
+
+#lalu ketik di terminal
+
+puts "git init"
+
+#kemudian remote git agar terhubung dengan repo anda di github
+
+puts "git remote add origin link-url repo anda"
+
+#cek dengan cara 
+
+puts "git remote -v"
+
+#cek apakah folder anda sudah terhubung dengan git
+
+puts "git status"
+
+#untuk membuat file siap di commit ketik 
+
+puts "git add -A"
+
+#agar file di commit
+
+puts "git commit -m 'Menambahkan file baru' "
+
+#step terakhir
+
+puts "git push origin master"
